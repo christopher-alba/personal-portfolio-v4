6128471d8395861eced77d0fe5984a11bbaf1bbb
Christopher Alba <chris.alba.dev@gmail.com>
2023-03-14 22:37:31 +1300

    setup styled components themes

---
+++ src/themes/globalStyles.ts
@@ -0,0 +1,62 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    overflow-x:hidden;
+    background: ${({ theme }) => theme.colors.primary};
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    color: ${({ theme }) => theme.colors.secondary} !important;
+    font-family:  'Inter', sans-serif;
+    & button{
+      font-family:  'Inter', sans-serif;
+    }
+    margin:0;
+    &::-webkit-scrollbar-track
+    {
+      background-color: ${({ theme }) => {
+        if (theme.name === "dark") {
+          return "#F5F5F5";
+        } else {
+          return "#2e2e2e";
+        }
+      }};
+    }
+
+    &::-webkit-scrollbar
+    {
+      width: 15px;
+      background-color: ${({ theme }) => {
+        if (theme.name === "dark") {
+          return "#F5F5F5";
+        } else {
+          return "#2e2e2e";
+        }
+      }};
+    }
+    &::-webkit-scrollbar-thumb
+      {
+          background-color: ${({ theme }) => {
+            if (theme.name === "dark") {
+              return "#d4d4d4";
+            } else {
+              return "black";
+            }
+          }};
+          border: 2px solid ${({ theme }) => {
+            if (theme.name === "dark") {
+              return "#d4d4d4";
+            } else {
+              return "#2e2e2e";
+            }
+          }};;
+          border-radius:10px;
+      }
+  }
+  button {
+    font-family: 'Inter', sans-serif;
+    text-transform: capitalize;
+  }
+`;
